dca19484a579b3bb42bcf519b0e281f483c75184
B11-AINF <B11@academico.unab.ad>
2022-10-18 09:28:22 -0500

    Se agrega el proyecto de JAVA-NETBEANS a git

---
+++ src/listase/ListasE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listase;
+
+/**
+ *
+ * @author B11
+ */
+public class ListasE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
